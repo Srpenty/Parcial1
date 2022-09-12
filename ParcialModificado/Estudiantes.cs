c7f1145760cc6a1968d8a1bdd61111d746c5d935
Oscar <Oscar@LAPTOP-2H49CU0U>
2022-09-12 10:17:00 -0600

    Parcial

---
+++ ParcialModificado/Estudiantes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParcialModificado
+{
+    class Estudiantes
+    {
+        private string _nombre;
+        private string _apellido;
+        private int _edad;
+        private double _promedio;
+
+        public Estudiantes(string nombre, string apellido, int edad, double promedio)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _edad = edad;
+            _promedio = promedio;
+        }
+
+        public string getdata()
+        {
+            return "Nombre: " + _nombre + " Apellido: " + _apellido + " Edad: " + _edad + " Nota: " + _promedio;
+        }
+    }
+}
---
+++ ParcialModificado/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+namespace ParcialModificado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Op = 0;
+            do
+            {
+                int numestudiantes;
+                ArrayList registro = new ArrayList();
+                string nombre;
+                string apellido;
+                int edad;
+                double nota1;
+                double nota2;
+                double parcial;
+                double promedio;
+                int contador = 0;
+                Console.WriteLine("Ingrese el numero de studiantes");
+                numestudiantes = int.Parse(Console.ReadLine());
+
+                while (contador < numestudiantes)
+                {
+                    Console.Write("Ingrese el nombre del estudiante: ");
+                    nombre = Console.ReadLine();
+                    Console.Write("Ingrese el apellido del estudiante: ");
+                    apellido = Console.ReadLine();
+                    Console.Write("Ingrese la edad del estudiante: ");
+                    edad = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la nota del estudiante: ");
+                    nota1 = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la segunda nota del estudiante: ");
+                    nota2 = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la nota del parcial del estudiante: ");
+                    parcial = int.Parse(Console.ReadLine());
+                    promedio = (nota1 * 0.3) + (nota2 * 0.3) + (parcial * 0.4);
+                    new Estudiantes(nombre, apellido, edad, promedio); 
+                    contador++;
+
+                    if (promedio >= 6)
+                    {
+                        Console.WriteLine("Aprobado");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Reprobado");
+                    }
+                }
+                foreach (Estudiantes estus in registro)
+                {
+                    Console.WriteLine(estus.getdata());
+                }
+                Console.WriteLine("Si desea agregar otro estudiante precione 0, si desea salir precione cualquier otro numero");
+                Op = int.Parse(Console.ReadLine());
+            } while (Op == 0);
+        }
+    }
+}
